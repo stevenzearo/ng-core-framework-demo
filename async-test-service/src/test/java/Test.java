ad1ff6f6cd778dc06a27351ea34db60043345307
steve <stevenzearo@gmail.com>
2019-10-15 18:14:50 +0800

    scheduler task test

--- async-test-service/src/test/java/Test.java
+++ async-test-service/src/test/java/Test.java
@@ -1,4 +1,3 @@
-import javax.swing.table.TableStringConverter;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -8,15 +7,27 @@ import java.util.concurrent.locks.ReentrantLock;
  * @author steve
  */
 public class Test {
-    abstract static class Scheduler extends TimerTask{
-        private Long intervalSeconds;
+    public static void main(String[] args) {
+        List<Integer> integers = List.of(1, 2, 3, 4);
+        integers.stream().filter(x -> x >= 2).forEach(System.out::print);
+        Timer timer = new Timer();
+        timer.schedule(new Scheduler(() -> System.out.println("hello, world!"), 3L), 300);
+    }
+
+    @FunctionalInterface
+    interface Task {
+        void run();
+    }
+
+    static class Scheduler extends TimerTask {
         private static final ReentrantLock lock = new ReentrantLock();
+        private Long intervalSeconds;
+        private Task task;
 
-        Scheduler(Long intervalSeconds) {
+        Scheduler(Task task, Long intervalSeconds) {
             Scheduler.this.intervalSeconds = intervalSeconds;
+            Scheduler.this.task = task;
         }
-
-        abstract public void scheduling();
 
         @Override
         public void run() {
@@ -27,21 +38,9 @@ public class Test {
                     e.printStackTrace();
                 }
                 lock.lock();
-                scheduling();
+                task.run();
                 lock.unlock();
             }
         }
-    }
-
-    public static void main(String[] args) {
-        List<Integer> integers = List.of(1, 2, 3, 4);
-        integers.stream().filter(x -> x >= 2).forEach(System.out::print);
-        Timer timer = new Timer();
-        timer.schedule(new Scheduler(3L) {
-            @Override
-            public void scheduling() {
-                System.out.println("hello, world");
-            }
-        }, 300);
     }
 }
